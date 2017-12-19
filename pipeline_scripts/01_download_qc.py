72660b87890cd1fcc502827ffc29ea36e8e9369f
Allison Shultz <allisonjshultz@gmail.com>
2017-12-19 17:08:18 -0500

    Added trimming, mapping etc. script writing

--- pipeline_scripts/01_download_qc.py
+++ pipeline_scripts/01_download_qc.py
@@ -59,10 +59,12 @@ def extract_config(config_filename):
     #Return objects    
     return(config_info)
 
+
 #Check if a directory exists. If not, create it.
 def directory_create(test_dir):
     dir = os.path.dirname("%s/"%test_dir)
     if not os.path.exists(dir):
+
         os.makedirs(dir)
 
 #Create generic slurm script
@@ -70,6 +72,7 @@ def script_create():
     slurm_script = '''#!/bin/bash\n#SBATCH -p {partition}\n#SBATCH -t {time}\n#SBATCH --mem {mem}\n#SBATCH -n {cores}\n#SBATCH -N {nodes}\n#SBATCH -J {jobid}\n#SBATCH -o logs/{jobid}_%j.out\n#SBATCH -e logs/{jobid}_%j.err\n\n{cmd}
     '''
     return(slurm_script)
+
     
 #Submit filename to slurm with sbatch, returns job id number
 def sbatch_submit(filename):
@@ -77,9 +80,11 @@ def sbatch_submit(filename):
     stdout,stderr = proc.communicate()
     return stdout.strip('Submitted batch job ')
 
-#Create an sbatch file for a given set of SRAs and split into fastq files
+
+#Create an sbatch file for a given set of SRAs and split into fastq files. Returns a list of new sbatch filenames
 def sra_download_sbatch(sp_dir,sample_ncbi_dict):
     slurm_script = script_create()
+    sra_dl_sbatch_filenames = []
     
     #Paths to various required software
     path_to_ascp="/n/home13/ashultz/.aspera/connect/bin/ascp"
@@ -89,8 +94,10 @@ def sra_download_sbatch(sp_dir,sample_ncbi_dict):
     
     for sample in sample_ncbi_dict.keys():
         for sra in sample_ncbi_dict[sample]:
+            print('Will download %s for sample %s, split, and run through fastqc'%(sra,sample))
     
-            cmd_1 = 'module load fastqc'
+            #Load modules and get versions for all programs used
+            cmd_1 = 'module load fastqc/0.11.5-fasrc01'
             cmd_2 = r'%sprefetch --force all --max-size 500000000 -a "%s|%s" --ascp-options "-QT -l 10G" %s'%(path_to_sratools,path_to_ascp,path_to_ascp_openssh,sra)
             cmd_3 = r'%sfastq-dump --outdir %s/fastq --gzip --split-files %s/sra/%s.sra'%(path_to_sratools,sp_dir,path_to_sra_dls,sra)
             cmd_4 = 'fastqc -o %s/fastqc %s/fastq/%s_1.fastq.gz %s/fastq/%s_2.fastq.gz'%(sp_dir,sp_dir,sra,sp_dir,sra) 
@@ -99,14 +106,17 @@ def sra_download_sbatch(sp_dir,sample_ncbi_dict):
     
     #Format sbatch script
             sra_script = slurm_script.format(partition="shared",time="1-0:00",mem="4000",cores="1",nodes="1",jobid="SRA",cmd=final_cmd)
-
-            out_file = open("%s/scripts/sra_download_parse_%s.sbatch"%(sp_dir,sra),"w")
+            out_filename = "%s/scripts/01_sra_download_parse_%s.sbatch"%(sp_dir,sra)
+            out_file = open(out_filename,"w")
             out_file.write(sra_script)
             out_file.close
-            #print(sra_script)
+            sra_dl_sbatch_filenames.append(out_filename)
+    
+    return(sra_dl_sbatch_filenames)
+            
 
 
-#Use FTP to download species genome fasta file. Will obtain FTP location by first downloading the current genbank assembly summary report.
+#Use FTP to download species genome fasta file. Will obtain FTP location by first downloading the current genbank assembly summary report. Returns name of sbatch script created.
 def get_ncbi_genome(sp_dir,species_name,sp_abbr):
 
     #Recreate genome directory
@@ -151,7 +161,7 @@ def get_ncbi_genome(sp_dir,species_name,sp_abbr):
             genome_ftp_path = possible_genome_opts[0][19]                
             genome_filename = '%s_genomic.fna.gz'%genome_ftp_path.split("/")[-1]
             full_genome_path = '%s/%s'%(genome_ftp_path,genome_filename)
-            print("\nDownloading %s genome accession %s from: %s\n\nCopying fasta file to %s.fa and indexing with samtools faidx and bwa index"%(species_name,possible_genome_opts[0][0],full_genome_path,sp_abbr))
+            print("\nDownloading %s genome accession %s from: %s\n\nCopying fasta file to %s.fa and indexing with samtools faidx and bwa index, and creating a sequence dictionary with samtools dict\n"%(species_name,possible_genome_opts[0][0],full_genome_path,sp_abbr))
 
         elif len(possible_genome_opts) > 1:
             sys.exit("There seems to be more than one representative genome for the species you provided, which is problematic. See the genbank assebmly summary table in the genome directory to refine genome name before proceeding, or supply a local fasta.")
@@ -160,28 +170,34 @@ def get_ncbi_genome(sp_dir,species_name,sp_abbr):
     
     slurm_script = script_create()
     
-    cmd_1 = 'module load samtools\nmodule load bwa'
+    #Load modules, also print samtools and bwa versions
+    cmd_1 = 'module load samtools/1.5-fasrc01\nmodule load bwa/0.7.9a-fasrc03'
     cmd_2 = 'wget -P %s %s'%(genome_dir,full_genome_path)
     cmd_3 = 'gunzip %s/%s'%(genome_dir,genome_filename)
     cmd_4 = 'mv %s/%s %s/%s.fa'%(genome_dir,genome_filename[:-3],genome_dir,sp_abbr)
     cmd_5 = 'samtools faidx %s/%s.fa'%(genome_dir,sp_abbr)
     cmd_6 = 'bwa index %s/%s.fa'%(genome_dir,sp_abbr)
+    cmd_7 = 'samtools dict -o %s/%s.dict %s/%s.fa'%(genome_dir,sp_abbr,genome_dir,sp_abbr)
     
-    final_cmd = "%s\n\n%s\n\n%s\n\n%s\n\n%s\n\n%s"%(cmd_1,cmd_2,cmd_3,cmd_4,cmd_5,cmd_6)    
+    final_cmd = "%s\n\n%s\n\n%s\n\n%s\n\n%s\n\n%s\n\n%s"%(cmd_1,cmd_2,cmd_3,cmd_4,cmd_5,cmd_6,cmd_7)    
     
     #Format sbatch script
     genome_script = slurm_script.format(partition="shared",time="0-8:00",mem="8000",cores="1",nodes="1",jobid="Genome_DL_Index",cmd=final_cmd)
 
-    out_file = open("%s/scripts/genome_download_index_%s.sbatch"%(sp_dir,sp_abbr),"w")
+    out_filename = "%s/scripts/02_genome_download_index_%s.sbatch"%(sp_dir,sp_abbr)
+    out_file = open(out_filename,"w")
     out_file.write(genome_script)
     out_file.close
+    
+    return(out_filename)
 
 
-#Make sure that genome and genome indexes are set up in genome directory for mapping
-def process_local_genome(sp_dir,genome_local,sp_abbr,genome_present,bwa_index_present,faidx_index_present):
+#Make sure that genome and genome indexes are set up in genome directory for mapping. Returns name of sbatch script created.
+def process_local_genome(sp_dir,genome_local,sp_abbr,genome_present,bwa_index_present,faidx_index_present,dict_index_present):
     genome_dir = "%s/genome"%(sp_dir)
     
-    cmd_1 = 'module load samtools\nmodule load bwa'
+    #Load modules, also print samtools and bwa versions
+    cmd_1 = 'module load samtools/1.5-fasrc01\nmodule load bwa/0.7.9a-fasrc03'
     
     #Copy genome if necessary
     if genome_present == False:
@@ -204,18 +220,67 @@ def process_local_genome(sp_dir,genome_local,sp_abbr,genome_present,bwa_index_pr
     else:
         cmd_4 = ''
         
-    final_cmd = "%s\n\n%s\n\n%s\n\n%s"%(cmd_1,cmd_2,cmd_3,cmd_4)
+    if dict_index_present == False:
+        print("\nCreating sequence dictionary file for %s/%s.fa"%(genome_dir,sp_abbr))
+        cmd_5 = 'samtools dict -o %s/%s.dict %s/%s.fa'%(genome_dir,sp_abbr,genome_dir,sp_abbr)
+    else:
+        cmd_5 = ''
+        
+    final_cmd = "%s\n\n%s\n\n%s\n\n%s\n\n%s"%(cmd_1,cmd_2,cmd_3,cmd_4,cmd_5)
     
     #Format sbatch script and write file
     slurm_script = script_create()
     genome_script = slurm_script.format(partition="shared",time="0-8:00",mem="8000",cores="1",nodes="1",jobid="Genome_CP_Index",cmd=final_cmd)
 
-    out_file = open("%s/scripts/genome_cp_index_%s.sbatch"%(sp_dir,sp_abbr),"w")
+    out_filename = "%s/scripts/02_genome_cp_index_%s.sbatch"%(sp_dir,sp_abbr)
+    out_file = open(out_filename,"w")
     out_file.write(genome_script)
     out_file.close
 
+    return(out_filename)
 
 
+#Create sbatch files for trimming, mapping, sorting, indexing and alignment stat creation for each set of paired SRA fastq files.
+def fastq_trim_align_stats(sp_dir,sra,sp_abbr,sample):
+    print('Will trim, map, sort, index, and collect alignment stats for %s from sample %s'%(sra,sample))
+
+    #Set read group info: ID = SRA number, SM = sample id, PU: SRA.sample, LB: sample id 
+    read_group_info = '@RG\\tID:%s\\tSM:%s\\tPU:%s.%s\\tLB:%s'%(sra,sample,sra,sample,sample)
+    path_to_picard = '~/sw/bin/picard.jar'
+
+    #Load necessary modules
+    cmd_1 = 'module load NGmerge/0.1-fasrc01\nmodule load bwa/0.7.9a-fasrc03\nmodule load java/1.8.0_45-fasrc01'
+    
+    #If paired end data:
+    #Trim adapters with NGmerge
+    cmd_2 = 'NGmerge -1 %s/fastq/%s_1.fastq.gz -2 %s/fastq/%s_2.fastq.gz -a -v -o %s/fastq/%s_trimmed -n 8 -z'%(sp_dir,sra,sp_dir,sra,sp_dir,sra)
+    
+    #Map to genome with BWA mem
+    cmd_3 = r"bwa mem -M -t 8 -R '%s' %s/fastq/%s_trimmed_1.fastq.gz %s/fastq/%s_trimmed_2.fastq.gz > %s/alignment/%s_bwa.sam"%(read_group_info,sp_dir,sra,sp_dir,sra,sp_dir,sra)
+    
+    cmd_4 = 'java -Mxm7g -XX:ParallelGCThreads=6 -jar %s SortSam I=%s/alignment/%s.bam O=%s/alignment/%s.sorted.bam SORT_ORDER=coordinate'%(path_to_picard,sp_dir,sra,sp_dir,sra)
+    
+    cmd_5 = 'java -Mxm7g -XX:ParallelGCThreads=6 -jar %s BuildBamIndex I=%s/alignment/%s.sorted.bam'%(path_to_picard,sp_dir,sra)
+    
+    #Calculate alignment summary stats
+    cmd_6 = 'java -Mxm7g -XX:ParallelGCThreads=6 -jar %s CollectAlignmentSummaryMetrics I=%s/alignment/%s.sorted.bam R=%s/genome/%s.fa METRIC_ACCUMULATION_LEVEL=SAMPLE O=%s/stats/%s.%s.alignment_metrics.txt'%(path_to_picard,sp_dir,sra,sp_dir,sp_abbr,sp_dir,sample,sra)
+
+    cmd_7 = 'if [%s/stats/%s.%s.alignment_metrics]\nrm %s/alignment/%s_bwa.sam \nfi'%(sp_dir,sample,sra,sp_dir,sra)
+    
+    cmd_list = [cmd_1,cmd_2,cmd_3,cmd_4,cmd_5,cmd_6,cmd_7]
+    
+    final_cmd = "\n\n".join(cmd_list)
+    
+    #Format sbatch script and write file
+    slurm_script = script_create()
+    genome_script = slurm_script.format(partition="shared",time="2-0:00",mem="16000",cores="8",nodes="1",jobid="Trim_Map",cmd=final_cmd)
+
+    out_filename = "%s/scripts/03_trim_map_stats_%s.sbatch"%(sp_dir,sra)
+    out_file = open(out_filename,"w")
+    out_file.write(genome_script)
+    out_file.close
+
+    return(out_filename)
 
 
 
@@ -241,11 +306,11 @@ def main():
     
     config_info = extract_config(config_filename)
 
-    #####Check if species directory, logs, scripts, fastq, fastqc, genome, and alignment directories, if not creates them one
+    #####Check if species directory, logs, scripts, fastq, fastqc, genome, alignment, and stats directories, if not creates them
     
     sp_dir = "%s/%s"%(config_info["out_dir"],config_info["abbv"])
     
-    print("\nOutput will be written to %s"%sp_dir)
+    print("\nOutput will be written to %s\n"%sp_dir)
     
     logs_dir = "%s/logs"%(sp_dir)
     scripts_dir = "%s/scripts"%(sp_dir)
@@ -253,6 +318,7 @@ def main():
     fastqc_dir = "%s/fastqc"%(sp_dir)
     genome_dir = "%s/genome"%(sp_dir)
     alignment_dir = "%s/alignment"%(sp_dir)
+    stats_dir = "%s/stats"%(sp_dir)
     
     directory_create(sp_dir)
     directory_create(logs_dir)
@@ -261,51 +327,68 @@ def main():
     directory_create(fastqc_dir)
     directory_create(genome_dir)
     directory_create(alignment_dir)
+    directory_create(stats_dir)
+    
     
     #####Download SRA files and use fastq-dump to split
     
     #Create sbatch files
-    sra_download_sbatch(sp_dir,config_info["sample_ncbi_dict"])
-    
+    sra_dl_sbatch_filenames = sra_download_sbatch(sp_dir,config_info["sample_ncbi_dict"])
     #Submit sbatch files - only allow up to 50 jobs to be running at one time.
+    
+    
     #use subprocess
     
 
     #####Prepare genome
     #Create sbatch file to download genome if not already present (checks for abbv.fa), mv to abbv.fa.
-    #e.g. ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/738/735/GCF_000738735.2_ASM73873v2/GCF_000738735.2_ASM73873v2_genomic.fna.gz
+    
+    genome_path = "%s/%s.fa"%(genome_dir,config_info["abbv"])
+    index_path_bwa = "%s/%s.fa.bwt"%(genome_dir,config_info["abbv"])
+    index_path_faidx = "%s/%s.fa.fai"%(genome_dir,config_info["abbv"])
+    index_path_dict = "%s/%s.dict"%(genome_dir,config_info["abbv"])
+    genome_jobnum = []
     
     if "genome_ncbi" in config_info:
         #Create sbatch script
-        get_ncbi_genome(sp_dir,config_info["genome_ncbi"],config_info["abbv"])
+        genome_sbatch_name = get_ncbi_genome(sp_dir,config_info["genome_ncbi"],config_info["abbv"])
+        
         #Submit sbatch script
+        #genome_jobid = sbatch_submit(genome_sbatch_name)
         
     elif "genome_local" in config_info:
         #Check if genome, BWA indexes, and faidx indexes already exist in genome directory. If not, create script to copy and index.
-        genome_path = "%s/%s.fa"%(genome_dir,config_info["abbv"])
         genome_present = os.path.isfile(genome_path)
-        index_path_bwa = "%s/%s.fa.bwt"%(genome_dir,config_info["abbv"])
         bwa_index_present = os.path.isfile(index_path_bwa)
-        index_path_faidx = "%s/%s.fa.fai"%(genome_dir,config_info["abbv"])
         faidx_index_present = os.path.isfile(index_path_faidx)
+        dict_index_present = os.path.isfile(index_path_dict)
 
         #Create sbatch script if any missing elements (genome, faidx or bwa index)
-        if genome_present == False or bwa_index_present == False or faidx_index_present == False:  
-                process_local_genome(sp_dir,config_info["genome_local"],config_info["abbv"],genome_present,bwa_index_present,faidx_index_present)
-        
+        if genome_present == False or bwa_index_present == False or faidx_index_present == False or dict_index_present == False:  
+            genome_sbatch_name = process_local_genome(sp_dir,config_info["genome_local"],config_info["abbv"],genome_present,bwa_index_present,faidx_index_present,dict_index_present)
+            
             #Submit sbatch script
+            #genome_job_id = sbatch_submit(genome_sbatch_name)
+        
+            
 
 
 
-    #Once SRA dl jobs are finished, check that all fastq files are there. If so, continue below. If not, 
+    #Once SRA dl and genome jobs are finished, check that all fastq files are there. If so, continue below. If not, 
     
     #Trim fastq files with NGmerge
-    #Map fastq files to genome with BWA
     #Set Read Group information
-    
-    
-
+    #Map fastq files to genome with BWA
+    #Sort (convert to BAM), Index
     #Calculate alignment stats
+    #Remove SAM file if stats file is there
+    mapping_jobids = []
+    
+    for sample in config_info["sample_ncbi_dict"]:
+        for sra in config_info["sample_ncbi_dict"][sample]:
+            sra_map_sbatchfile = fastq_trim_align_stats(sp_dir,sra,config_info["abbv"],sample)
+            #sra_map_jobid = sbatch_submit(sra_map_sbatchfile)
+            #mapping_jobids.append(sra_map_jobid)
 
 if __name__ == "__main__":
     main()

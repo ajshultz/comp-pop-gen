cbb29f5465512fdc462cec457e39aa7a3ffeb2e2
Allison Shultz <allisonjshultz@gmail.com>
2017-12-20 12:43:22 -0500

    Bug fixes + genome job submission + tracking

--- pipeline_scripts/01_download_qc.py
+++ pipeline_scripts/01_download_qc.py
@@ -7,6 +7,7 @@ import sys
 import os
 import argparse
 from subprocess import Popen,PIPE
+from time import sleep
 
 #Extract Sample, SRA and genome info from config file. Sample data will be stored as a dictionary with sample ID as keys and a list of SRA accessions as values. Returns a dictionary of this info.
 def extract_config(config_filename):
@@ -69,7 +70,7 @@ def directory_create(test_dir):
 
 #Create generic slurm script
 def script_create():
-    slurm_script = '''#!/bin/bash\n#SBATCH -p {partition}\n#SBATCH -t {time}\n#SBATCH --mem {mem}\n#SBATCH -n {cores}\n#SBATCH -N {nodes}\n#SBATCH -J {jobid}\n#SBATCH -o logs/{jobid}_%j.out\n#SBATCH -e logs/{jobid}_%j.err\n\n{cmd}
+    slurm_script = '''#!/bin/bash\n#SBATCH -p {partition}\n#SBATCH -t {time}\n#SBATCH --mem {mem}\n#SBATCH -n {cores}\n#SBATCH -N {nodes}\n#SBATCH -J {jobid}\n#SBATCH -o {sp_dir}/logs/{jobid}_%j.out\n#SBATCH -e {sp_dir}/logs/{jobid}_%j.err\n\n{cmd}
     '''
     return(slurm_script)
 
@@ -80,6 +81,38 @@ def sbatch_submit(filename):
     stdout,stderr = proc.communicate()
     return stdout.strip('Submitted batch job ')
 
+
+#Check job status of specific jobid: returns job status
+def jobid_status(jobid):
+    proc = Popen('sacct --format state --noheader -j %d'%int(jobid),shell=True,stdout=PIPE,stderr=PIPE)
+    stdout,stderr=proc.communicate()
+    if proc.returncode != 0:
+        raise Exception('Error running sacct: %s'%stderr)
+    if stdout.strip() == '':
+        return("No Status")
+    lines = stdout.split()
+    return(lines[0].decode("utf-8","ignore"))
+    
+    
+#Check the status of all jobs, returns dictionary of jobid:status. Ignores jobs with ".ba+" and ".ex+" file extensions.
+def all_jobs_status():
+    proc = Popen('sacct --format jobid,state --noheader',shell=True,stdout=PIPE,stderr=PIPE)
+    stdout,stderr=proc.communicate()
+    stdout=stdout.decode("utf-8","ignore")
+    stderr=stderr.decode("utf-8","ignore")
+    if proc.returncode != 0:
+        raise Exception('Error running sacct: %s'%stderr)
+    if stdout.strip() == '':
+        return("No Status")
+    lines = stdout.split("\n")
+    status_dict = {}
+    for line in lines:
+        line = line.split()
+        if len(line) > 1:
+            if not re.search('.ba+',line[0]) and not re.search('.ex+',line[0]):
+                status_dict[line[0]]=line[1]
+    return(status_dict)
+    
 
 #Create an sbatch file for a given set of SRAs and split into fastq files. Returns a list of new sbatch filenames
 def sra_download_sbatch(sp_dir,sample_ncbi_dict):
@@ -105,7 +138,7 @@ def sra_download_sbatch(sp_dir,sample_ncbi_dict):
             final_cmd = "%s\n\n%s\n\n%s\n\n%s"%(cmd_1,cmd_2,cmd_3,cmd_4)
     
     #Format sbatch script
-            sra_script = slurm_script.format(partition="shared",time="1-0:00",mem="4000",cores="1",nodes="1",jobid="SRA",cmd=final_cmd)
+            sra_script = slurm_script.format(partition="shared",time="1-0:00",mem="4000",cores="1",nodes="1",jobid="SRA",sp_dir=sp_dir,cmd=final_cmd)
             out_filename = "%s/scripts/01_sra_download_parse_%s.sbatch"%(sp_dir,sra)
             out_file = open(out_filename,"w")
             out_file.write(sra_script)
@@ -171,7 +204,7 @@ def get_ncbi_genome(sp_dir,species_name,sp_abbr):
     slurm_script = script_create()
     
     #Load modules, also print samtools and bwa versions
-    cmd_1 = 'module load samtools/1.5-fasrc01\nmodule load bwa/0.7.9a-fasrc03'
+    cmd_1 = 'module load samtools/1.5-fasrc01\nmodule load bwa/0.7.15-fasrc01'
     cmd_2 = 'wget -P %s %s'%(genome_dir,full_genome_path)
     cmd_3 = 'gunzip %s/%s'%(genome_dir,genome_filename)
     cmd_4 = 'mv %s/%s %s/%s.fa'%(genome_dir,genome_filename[:-3],genome_dir,sp_abbr)
@@ -182,7 +215,7 @@ def get_ncbi_genome(sp_dir,species_name,sp_abbr):
     final_cmd = "%s\n\n%s\n\n%s\n\n%s\n\n%s\n\n%s\n\n%s"%(cmd_1,cmd_2,cmd_3,cmd_4,cmd_5,cmd_6,cmd_7)    
     
     #Format sbatch script
-    genome_script = slurm_script.format(partition="shared",time="0-8:00",mem="8000",cores="1",nodes="1",jobid="Genome_DL_Index",cmd=final_cmd)
+    genome_script = slurm_script.format(partition="shared",time="0-8:00",mem="8000",cores="1",nodes="1",jobid="Genome_DL_Index",sp_dir=sp_dir,cmd=final_cmd)
 
     out_filename = "%s/scripts/02_genome_download_index_%s.sbatch"%(sp_dir,sp_abbr)
     out_file = open(out_filename,"w")
@@ -197,7 +230,7 @@ def process_local_genome(sp_dir,genome_local,sp_abbr,genome_present,bwa_index_pr
     genome_dir = "%s/genome"%(sp_dir)
     
     #Load modules, also print samtools and bwa versions
-    cmd_1 = 'module load samtools/1.5-fasrc01\nmodule load bwa/0.7.9a-fasrc03'
+    cmd_1 = 'module load samtools/1.5-fasrc01\nmodule load bwa/0.7.15-fasrc01'
     
     #Copy genome if necessary
     if genome_present == False:
@@ -230,7 +263,7 @@ def process_local_genome(sp_dir,genome_local,sp_abbr,genome_present,bwa_index_pr
     
     #Format sbatch script and write file
     slurm_script = script_create()
-    genome_script = slurm_script.format(partition="shared",time="0-8:00",mem="8000",cores="1",nodes="1",jobid="Genome_CP_Index",cmd=final_cmd)
+    genome_script = slurm_script.format(partition="shared",time="0-8:00",mem="8000",cores="1",nodes="1",jobid="Genome_CP_Index",sp_dir=sp_dir,cmd=final_cmd)
 
     out_filename = "%s/scripts/02_genome_cp_index_%s.sbatch"%(sp_dir,sp_abbr)
     out_file = open(out_filename,"w")
@@ -249,31 +282,29 @@ def fastq_trim_align_stats(sp_dir,sra,sp_abbr,sample):
     path_to_picard = '~/sw/bin/picard.jar'
 
     #Load necessary modules
-    cmd_1 = 'module load NGmerge/0.1-fasrc01\nmodule load bwa/0.7.9a-fasrc03\nmodule load java/1.8.0_45-fasrc01'
+    cmd_1 = 'module load NGmerge/0.2-fasrc01\nmodule load bwa/0.7.15-fasrc01\nmodule load java/1.8.0_45-fasrc01'
     
     #If paired end data:
     #Trim adapters with NGmerge
-    cmd_2 = 'NGmerge -1 %s/fastq/%s_1.fastq.gz -2 %s/fastq/%s_2.fastq.gz -a -v -o %s/fastq/%s_trimmed -n 8 -z'%(sp_dir,sra,sp_dir,sra,sp_dir,sra)
+    cmd_2 = 'NGmerge -1 %s/fastq/%s_1.fastq.gz -2 %s/fastq/%s_2.fastq.gz -a -v -o %s/fastq/%s_trimmed -n 8 -z -u 60'%(sp_dir,sra,sp_dir,sra,sp_dir,sra)
     
     #Map to genome with BWA mem
-    cmd_3 = r"bwa mem -M -t 8 -R '%s' %s/fastq/%s_trimmed_1.fastq.gz %s/fastq/%s_trimmed_2.fastq.gz > %s/alignment/%s_bwa.sam"%(read_group_info,sp_dir,sra,sp_dir,sra,sp_dir,sra)
+    cmd_3 = r"bwa mem -M -t 8 -R '%s' %s/genome/%s.fa %s/fastq/%s_trimmed_1.fastq.gz %s/fastq/%s_trimmed_2.fastq.gz > %s/alignment/%s_bwa.sam"%(read_group_info,sp_dir,sp_abbr,sp_dir,sra,sp_dir,sra,sp_dir,sra)
     
-    cmd_4 = 'java -Mxm7g -XX:ParallelGCThreads=6 -jar %s SortSam I=%s/alignment/%s.bam O=%s/alignment/%s.sorted.bam SORT_ORDER=coordinate'%(path_to_picard,sp_dir,sra,sp_dir,sra)
-    
-    cmd_5 = 'java -Mxm7g -XX:ParallelGCThreads=6 -jar %s BuildBamIndex I=%s/alignment/%s.sorted.bam'%(path_to_picard,sp_dir,sra)
+    cmd_4 = 'java -Xmx7g -XX:ParallelGCThreads=6 -jar %s SortSam I=%s/alignment/%s_bwa.sam O=%s/alignment/%s.sorted.bam SORT_ORDER=coordinate CREATE_INDEX=true'%(path_to_picard,sp_dir,sra,sp_dir,sra)
     
     #Calculate alignment summary stats
-    cmd_6 = 'java -Mxm7g -XX:ParallelGCThreads=6 -jar %s CollectAlignmentSummaryMetrics I=%s/alignment/%s.sorted.bam R=%s/genome/%s.fa METRIC_ACCUMULATION_LEVEL=SAMPLE O=%s/stats/%s.%s.alignment_metrics.txt'%(path_to_picard,sp_dir,sra,sp_dir,sp_abbr,sp_dir,sample,sra)
+    cmd_5 = 'java -Xmx7g -XX:ParallelGCThreads=6 -jar %s CollectAlignmentSummaryMetrics I=%s/alignment/%s.sorted.bam R=%s/genome/%s.fa METRIC_ACCUMULATION_LEVEL=SAMPLE O=%s/stats/%s.%s.alignment_metrics.txt'%(path_to_picard,sp_dir,sra,sp_dir,sp_abbr,sp_dir,sample,sra)
 
-    cmd_7 = 'if [%s/stats/%s.%s.alignment_metrics]\nrm %s/alignment/%s_bwa.sam \nfi'%(sp_dir,sample,sra,sp_dir,sra)
+    cmd_6 = 'if [ -f %s/stats/%s.%s.alignment_metrics.txt ]\nthen\n rm %s/alignment/%s_bwa.sam \nfi'%(sp_dir,sample,sra,sp_dir,sra)
     
-    cmd_list = [cmd_1,cmd_2,cmd_3,cmd_4,cmd_5,cmd_6,cmd_7]
+    cmd_list = [cmd_1,cmd_2,cmd_3,cmd_4,cmd_5,cmd_6]
     
     final_cmd = "\n\n".join(cmd_list)
     
     #Format sbatch script and write file
     slurm_script = script_create()
-    genome_script = slurm_script.format(partition="shared",time="2-0:00",mem="16000",cores="8",nodes="1",jobid="Trim_Map",cmd=final_cmd)
+    genome_script = slurm_script.format(partition="shared",time="1-0:00",mem="16000",cores="8",nodes="1",jobid="Trim_Map",sp_dir=sp_dir,cmd=final_cmd)
 
     out_filename = "%s/scripts/03_trim_map_stats_%s.sbatch"%(sp_dir,sra)
     out_file = open(out_filename,"w")
@@ -330,16 +361,6 @@ def main():
     directory_create(stats_dir)
     
     
-    #####Download SRA files and use fastq-dump to split
-    
-    #Create sbatch files
-    sra_dl_sbatch_filenames = sra_download_sbatch(sp_dir,config_info["sample_ncbi_dict"])
-    #Submit sbatch files - only allow up to 50 jobs to be running at one time.
-    
-    
-    #use subprocess
-    
-
     #####Prepare genome
     #Create sbatch file to download genome if not already present (checks for abbv.fa), mv to abbv.fa.
     
@@ -370,8 +391,37 @@ def main():
             #Submit sbatch script
             #genome_job_id = sbatch_submit(genome_sbatch_name)
         
-            
+    #Sleep 30 seconds to give job status time to get to sacct before starting to check    
+    sleep(30)
+    
+    dones = ['COMPLETED','CANCELLED','FAILED','TIMEOUT','PREEMPTED','NODE_FAIL']
+    #Check job id status of genome job. If not in one of the 'done' job status categories, wait 30 seconds and check again.
+    while jobid_status(genome_job_id) not in dones:
+        print("Genome not done yet")
+        sleep(30)
+    
+    #Check to make sure job completed, and that all necessary files are present. If not, exit and give information.
+    genome_job_completion_status = jobid_status(genome_job_id)
+    if genome_job_completion_status != 'COMPLETED':
+        sys.exit("There was a problem downloading and indexing the genome. The job exited with status %s. Please diagnose and fix before moving on"%genome_job_completion_status)
+        
+    genome_present = os.path.isfile(genome_path)
+    bwa_index_present = os.path.isfile(index_path_bwa)
+    faidx_index_present = os.path.isfile(index_path_faidx)
+    dict_index_present = os.path.isfile(index_path_dict)
+    if genome_present == False or bwa_index_present == False or faidx_index_present == False or dict_index_present == False:
+        sys.exit("The genome job finished but not all files and indexes are present. Please check, create missing indexes, and resubmit with local genome")
 
+    print("\nGenome download and indexing successfully finished\n")
+
+    #####Create sbatch files to download SRA files and use fastq-dump to split
+    
+    #Create sbatch files
+    sra_dl_sbatch_filenames = sra_download_sbatch(sp_dir,config_info["sample_ncbi_dict"])
+        
+    #Submit SRA read sbatch files            
+    sra_dl_jobids = []
+    
 
 
     #Once SRA dl and genome jobs are finished, check that all fastq files are there. If so, continue below. If not, 

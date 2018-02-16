d2cd6f3828e63f595af8fdb199161d35acf525b6
Allison Shultz <allisonjshultz@gmail.com>
2018-02-16 15:02:06 -0500

    added PL readgroup info, can remove once all current datasets run through

--- pipeline_scripts/02_dedup_gather_metrics.py
+++ pipeline_scripts/02_dedup_gather_metrics.py
@@ -135,7 +135,7 @@ def num_pend_run(job_id_list,date):
 #Create an sbatch file for a given set of SRAs and split into fastq files. Returns a list of new sbatch filenames
 def dedup_sbatch(sp_dir,sp_abbr,sample_ncbi_dict):
     slurm_script = script_create()
-    sra_dl_sbatch_filenames = []
+    dedup_sbatch_filenames = []
      
     for sample in sample_ncbi_dict.keys():
         #First check if dedup file is already present (already downloaded), or final BAM file already present. If it has, print statment and continue with next sample. 
@@ -152,9 +152,20 @@ def dedup_sbatch(sp_dir,sp_abbr,sample_ncbi_dict):
             ##For now, using my own installation of GATK as it is not yet installed on the cluster
             cmd_1 = 'module load java/1.8.0_45-fasrc01'
             
+
+            #Add PL read group info (can remove once all initial libraries done with script 2)
+            rg_cmd_list = []
+            for sra in sample_ncbi_dict[sample]:
+                
+                rg_cmd = 'gatk --java-options "-Xmx8g -XX:ParallelGCThreads=1" AddOrReplaceReadGroups -I %s/alignment/%s.sorted.bam -O %s/alignment/%s.sorted.rg.bam -ID %s -SM %s -PU %s.%s -LB %s -PL illumina --COMPRESSION_LEVEL 5 --CREATE_INDEX true'%(sp_dir,sra,sp_dir,sra,sra,sample,sra,sample,sample)
+                
+                rg_cmd_list.append(rg_cmd)
+            
+            rg_cmd = "\n\n".join(rg_cmd_list)
+            
             #Create GATK mark duplicates command
             gatk_cmd_1 = 'gatk --java-options "-Xmx8g -XX:ParallelGCThreads=1" MarkDuplicatesGATK '
-            gatk_cmd_2 = ('-I '+align_dir+'%s.sorted.bam ')*len(sample_ncbi_dict[sample])%tuple(sample_ncbi_dict[sample])
+            gatk_cmd_2 = ('-I '+align_dir+'%s.sorted.rg.bam ')*len(sample_ncbi_dict[sample])%tuple(sample_ncbi_dict[sample])
             gatk_cmd_3 = '-O %s/dedup/%s.dedup.bam '%(sp_dir,sample)
             gatk_cmd_4 = '--METRICS_FILE %s/stats/%s.dedup.metrics.txt '%(sp_dir,sample)
             gatk_cmd_5 = '--COMPRESSION_LEVEL 5'
@@ -163,7 +174,7 @@ def dedup_sbatch(sp_dir,sp_abbr,sample_ncbi_dict):
             cmd_2 = "".join([gatk_cmd_1,gatk_cmd_2,gatk_cmd_3,gatk_cmd_4,gatk_cmd_5])
             
             #Sort and index dedup bam file
-            cmd_3 = 'gatk --java-options "-Xmx8g -XX:ParallelGCThreads=1" SortSam -I %s/dedup/%s.dedup.bam -O %s/dedup/%s.dedup.sorted.bam --SORT_ORDER coordinate --CREATE_INDEX true --COMPRESSION_LEVEL 5'%(sp_dir,sample,sp_dir,sample)
+            cmd_3 = 'gatk --java-options "-Xmx8g -XX:ParallelGCThreads=1" SortSam -I %s/dedup/%s.dedup.rg.bam -O %s/dedup/%s.dedup.sorted.bam --SORT_ORDER coordinate --CREATE_INDEX true --COMPRESSION_LEVEL 5'%(sp_dir,sample,sp_dir,sample)
             
             #Validate sorted bam
             cmd_4 = 'gatk --java-options "-Xmx8g -XX:ParallelGCThreads=1" ValidateSamFile -I %s/dedup/%s.dedup.sorted.bam -O %s/stats/%s.validate.txt'%(sp_dir,sample,sp_dir,sample)
@@ -174,7 +185,7 @@ def dedup_sbatch(sp_dir,sp_abbr,sample_ncbi_dict):
             #Grab only genome output
             cmd_6 = r"""awk '$1 == "genome" {print $0}' %s/stats/%s.coverage > %s/stats/%s.genome.coverage"""%(sp_dir,sample,sp_dir,sample)
         
-            cmd_list = [cmd_1,cmd_2,cmd_3,cmd_4,cmd_5,cmd_6]
+            cmd_list = [cmd_1,rg_cmd,cmd_2,cmd_3,cmd_4,cmd_5,cmd_6]
 
             final_cmd = "\n\n".join(cmd_list)
 
@@ -185,9 +196,9 @@ def dedup_sbatch(sp_dir,sp_abbr,sample_ncbi_dict):
             out_file = open(out_filename,"w")
             out_file.write(sra_script)
             out_file.close
-            sra_dl_sbatch_filenames.append(out_filename)
+            dedup_sbatch_filenames.append(out_filename)
     
-    return(sra_dl_sbatch_filenames)
+    return(dedup_sbatch_filenames)
 
 
 
@@ -254,7 +265,9 @@ def main():
             print("Dedup, sort and validate job %s failed with code: %s"%(job,completed_jobids[job]))
     
     #####Collate summary statistics   
-    #
+    #Collect alignment metrics, dedup metrics
+    
+    #Collect coverage histograms, plot in pdf (species_date.pdf), table of mean coverage
     
             
     #Check that the final sorted bam and index is available, if so, remove intermediate files (dedup)

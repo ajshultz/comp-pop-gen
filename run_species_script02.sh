984ce7b626754e00d76a16e2b52b1521ff23a934
Allison Shultz <allisonjshultz@gmail.com>
2018-02-28 13:11:11 -0500

    wrong file name

--- run_species_script02.sh
+++ run_species_script02.sh
@@ -4,5 +4,5 @@
 
 for SPECIES in Ccornix Corientalis Cbrachyrhynchos Cmonedula Cpectoralis Cfrugilegus Cdauuricus Pcollybita Ptristis Fhypoleuca Fspeculigera Pmajor Pmontanus Pdomesticus Pitaliae Phispaniolensis Tbichenovii Vchrysoptera Vcyanoptera
 do
-python ../comp-pop-gen/pipeline_scripts/02_download_qc.py --config ../comp-pop-gen/config_files/${SPECIES}_config.txt >> python_logs/${SPECIES}.log
+python ../comp-pop-gen/pipeline_scripts/02_dedup_gather_metrics.py --config ../comp-pop-gen/config_files/${SPECIES}_config.txt >> python_logs/${SPECIES}.log
 done

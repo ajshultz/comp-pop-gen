ddde8871f022a972eae97b37ce6ef53e7ef9799d
Allison Shultz <allisonjshultz@gmail.com>
2018-03-28 12:14:53 -0400

    unindented

--- variant_calling_testing/03_haplotypecalling_testing.py
+++ variant_calling_testing/03_haplotypecalling_testing.py
@@ -525,7 +525,7 @@ def main():
         sample_files = [name for name in os.listdir(gvcf_dir) if sample in name]
         #Can look for .tbi files
     
-            haplotypecaller_sbatch(sp_dir,sp_abbr=config_info["abbv"],sample=sample,coverage=config_info["coverage"],het=config_info["het"],memory_hc=config_info["memory_hc"],nintervals=config_info["nintervals"],pipeline=config_info["pipeline"])
+        haplotypecaller_sbatch(sp_dir,sp_abbr=config_info["abbv"],sample=sample,coverage=config_info["coverage"],het=config_info["het"],memory_hc=config_info["memory_hc"],nintervals=config_info["nintervals"],pipeline=config_info["pipeline"])
     
     ###Submit all sbatch scripts, include full array #s, memory, time
     #Keep track of sample + array_job_ids    

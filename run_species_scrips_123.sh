1d3f5db7203c244e725a46d58ee71edc8e264c42
Allison Shultz <allisonjshultz@gmail.com>
2018-07-13 10:27:16 -0400

    added echo statement

--- run_species_scrips_123.sh
+++ run_species_scrips_123.sh
@@ -9,4 +9,6 @@ python ../comp-pop-gen/pipeline_scripts/01_download_qc.py --config ../comp-pop-g
 python ../comp-pop-gen/pipeline_scripts/02_dedup_gather_metrics.py --config ../comp-pop-gen/config_files/${SPECIES}_config.txt >> python_logs/${SPECIES}.log
 
 python ../comp-pop-gen/pipeline_scripts/03_haplotypecalling.py --config ../comp-pop-gen/config_files/${SPECIES}_config.txt >> python_logs/${SPECIES}.log
+
+echo ${SPECIES} is finished
 done

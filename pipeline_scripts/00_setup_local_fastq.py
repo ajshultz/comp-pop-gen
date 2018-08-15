b97a4252f5351c6c22219a826ac89d969820a382
Allison Shultz <allisonjshultz@gmail.com>
2018-08-15 13:55:31 -0400

    type error for writing config file

--- pipeline_scripts/00_setup_local_fastq.py
+++ pipeline_scripts/00_setup_local_fastq.py
@@ -37,7 +37,7 @@ def write_main_config(config_local, config_out, abbv):
         for line in config_local:
             line=line.split("\t")
             #path = line[0], read = line[1], sample ID = line[2], accession = line[3]
-            if line[1] == 1:
+            if line[1] == '1':
                 print("--SAMPLE_LOCAL", line[2], line[3], sep=" ", file=f)
     
 #Copy and rename fastqs

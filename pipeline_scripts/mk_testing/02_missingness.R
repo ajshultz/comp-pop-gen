455e28fb041f0a18fed619d82c731fbdb9f424ca
Sara JS Wuitchik <50593708+sjswuitchik@users.noreply.github.com>
2019-07-01 17:05:20 -0400

    Update 02_missingness.R
    Added footnote

--- pipeline_scripts/mk_testing/02_missingness.R
+++ pipeline_scripts/mk_testing/02_missingness.R
@@ -9,3 +9,5 @@ corCor.threshold <- 3*median(corCor.clean$Missing)
 corCor.remove <- corCor.clean %>% 
   filter(Missing >= corCor.threshold)
 write.csv(corCor.remove %>% select(INDV), "corCor_remove.csv")
+
+## if there are individuals to remove, rename corCor_remove.csv to *.indv, use vcftools --vcf _____ --remove-indv _____.indv --recode --recode-INFO-all --out ______ to remove individuals with high relative missingness

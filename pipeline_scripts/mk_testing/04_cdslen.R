e1ccd36509d1642214820b80f36f969c08d56f6c
Sara JS Wuitchik <50593708+sjswuitchik@users.noreply.github.com>
2019-08-28 13:21:17 -0400

    Update 04_cdslen.R

--- pipeline_scripts/mk_testing/04_cdslen.R
+++ pipeline_scripts/mk_testing/04_cdslen.R
@@ -9,3 +9,11 @@ cds.len <- cds.temp %>%
   summarise(cds.len = sum(cds.temp))
 write.table(cds.len, "cdslen.txt", sep = "\t", row.names = F, quote = F)
 
+corCor <- read.delim("corCor.ann.bed", header = F, sep = "\t", stringsAsFactors = F, quote = "") %>% 
+  as_tibble()
+colnames(corCor) <- c("#chr","start.pos","end.pos","id","qual","ref","alt","filter","info","i","ii","iii","iv","v","vi")
+corCor <- corCor %>% select(-c(id, qual, ref, alt, filter, i, ii, iii, iv, v, vi))
+corCor.clean <- data %>% separate(info, into = c(NA, "effect"), sep = "([\\|])")
+write.table(corCor.clean, "corCor.vcfann.bed", sep = "\t", row.names = F, quote = F)
+
+## NB: the cleaning of the annotated vcf will be streamlined with cyvcf2 by Aug 29

2a2ad8cbb61bf5f1a1c43c919b62b5a6ba4040d2
Sara JS Wuitchik <50593708+sjswuitchik@users.noreply.github.com>
2019-08-28 13:20:10 -0400

    Create 04_cdslen.R

---
+++ pipeline_scripts/mk_testing/04_cdslen.R
@@ -0,0 +1,11 @@
+rm(list=ls(all=TRUE))
+cds <- read.delim("corCor.onlyCDS.genes.bed", sep = "\t", header = F, stringsAsFactors = F, quote = "") %>%
+  as_tibble()
+colnames(cds) <- c("chr", "start", "end", "gene")
+cds.temp <- cds %>%
+  mutate(cds.temp = end - start)
+cds.len <- cds.temp %>%
+  group_by(gene) %>% 
+  summarise(cds.len = sum(cds.temp))
+write.table(cds.len, "cdslen.txt", sep = "\t", row.names = F, quote = F)
+

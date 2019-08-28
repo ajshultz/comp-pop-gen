a629a03c95da43ee6338417372afb52e3c2c765d
Sara JS Wuitchik <50593708+sjswuitchik@users.noreply.github.com>
2019-08-28 16:14:33 -0400

    Create 04_cyvcf2.py

---
+++ pipeline_scripts/mk_testing/04_cyvcf2.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python -tt
+
+import re
+import sys
+import os
+from cyvcf2 import VCF
+# see https://brentp.github.io/cyvcf2/docstrings.html
+
+def main():
+
+	vcf = VCF('/scratch/swuitchik/CompPopGen/consensus_ingroups/corCor/corCor.ann.vcf', gts012=True) # gts012=True makes value of 3 UNKNOWN, with 0,1,2 corresponding to numb ALT alleles
+
+	outFile = open("corCor.effects.txt", 'w')
+  
+	for variant in vcf:
+		# check if this site passed GATK VCF filtering, and only look at bi-allelic sites
+		if len(variant.ALT) == 1:
+			print(variant.CHROM, variant.end, sep="\t", end="\t", file=outFile)
+			for field in variant.INFO:
+				if field[0] == 'ANN':
+					annotations = field[1].split(",")
+					effects = []
+					for i in annotations:
+						i = i.split('|')
+						effects.append(i[1])
+					print(",".join(effects), file=outFile)
+
+	outFile.close()
+if __name__ == '__main__':
+  main()
+
+
+
+
+## Written by Brian J Arnold (barnold@harvard.edu)
+

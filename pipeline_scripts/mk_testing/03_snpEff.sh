5b5737ad86e410cb5ba86ac8730f7d77cb3223df
Sara JS Wuitchik <50593708+sjswuitchik@users.noreply.github.com>
2019-08-28 13:17:53 -0400

    Updated with BED conversion

--- pipeline_scripts/mk_testing/03_snpEff.sh
+++ pipeline_scripts/mk_testing/03_snpEff.sh
@@ -16,3 +16,13 @@ corCor.2.ncbi.genome : Corvus_cornix_cornix
 
 java -jar snpEff.jar build -gff3 -v corCor.2.ncbi #builds database
 java -jar snpEff.jar corCor.2.ncbi data/corCor.clean.vcf.gz > corCor.ann.vcf #annotates VCF
+
+## 
+# cp genes.gff.gz and corCor.ann.vcf to a new working directory
+
+mv genes.gff.gz corCor.genes.gff.gz
+gunzip corCor.genes.gff.gz 
+grep "CDS" corCor.genes.gff > corCor.onlyCDS.gff
+convert2bed -i gff < corCor.onlyCDS.gff > corCor.onlyCDS.gff.bed
+cat corCor.onlyCDS.gff.bed | python corCor.genenames.py > corCor.onlyCDS.genes.bed
+convert2bed -i vcf < corCor.ann.vcf > corCor.clean.ann.bed

4dc423b11538c05d9f484b27f619a4150baa7b16
Sara JS Wuitchik <50593708+sjswuitchik@users.noreply.github.com>
2019-08-28 16:14:52 -0400

    Update 03_snpEff.sh

--- pipeline_scripts/mk_testing/03_snpEff.sh
+++ pipeline_scripts/mk_testing/03_snpEff.sh
@@ -25,4 +25,3 @@ gunzip corCor.genes.gff.gz
 grep "CDS" corCor.genes.gff > corCor.onlyCDS.gff
 convert2bed -i gff < corCor.onlyCDS.gff > corCor.onlyCDS.gff.bed
 cat corCor.onlyCDS.gff.bed | python corCor.genenames.py > corCor.onlyCDS.genes.bed
-convert2bed -i vcf < corCor.ann.vcf > corCor.clean.ann.bed

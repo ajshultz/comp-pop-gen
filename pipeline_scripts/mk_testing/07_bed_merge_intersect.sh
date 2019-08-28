11c4396d670e102cb40d0880ad5ceb1c0f86695f
Sara JS Wuitchik <50593708+sjswuitchik@users.noreply.github.com>
2019-08-28 13:10:45 -0400

    Delete 07_bed_merge_intersect.sh

--- pipeline_scripts/mk_testing/07_bed_merge_intersect.sh
+++
@@ -1,5 +0,0 @@
-## Merge and intersect callable regions
-
-bedtools merge -i corCor.onlyCDS.gff.bed -c 10 -o distinct > corCor.onlyCDS.merge.gff.bed
-bedtools intersect -a corCor.onlyCDS.merge.gff.bed -b vcfout.bed > corCor.consensus.bed
-

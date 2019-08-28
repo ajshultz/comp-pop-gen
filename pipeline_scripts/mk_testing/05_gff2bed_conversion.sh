31d410946c9f825d4b41d7766e2afdd87176222b
Sara JS Wuitchik <50593708+sjswuitchik@users.noreply.github.com>
2019-08-28 13:10:24 -0400

    Delete 05_gff2bed_conversion.sh

--- pipeline_scripts/mk_testing/05_gff2bed_conversion.sh
+++
@@ -1,6 +0,0 @@
-## Extraction of CDS regions & conversion to BED file
-
-mv sequences.fa corCor.sequences.fa
-cat corCor.sequences.fa | bcftools consensus corCor.clean.ann.vcf.gz -o vcfout.fasta
-grep "CDS" corCor.genes.gff > corCor.onlyCDS.gff
-convert2bed -i gff < corCor.onlyCDS.gff > corCor.onlyCDS.gff.bed

28df895245ebdb451eda12cab5331a90349a159f
Sara JS Wuitchik <50593708+sjswuitchik@users.noreply.github.com>
2019-08-29 15:09:53 -0400

    Update 06_consensusbed.sh

--- pipeline_scripts/mk_testing/06_consensusbed.sh
+++ pipeline_scripts/mk_testing/06_consensusbed.sh
@@ -1,3 +1,11 @@
+# cp genes.gff.gz and corCor.ann.vcf to a new working directory
+
+mv genes.gff.gz corCor.genes.gff.gz
+gunzip corCor.genes.gff.gz 
+grep "CDS" corCor.genes.gff > corCor.onlyCDS.gff
+convert2bed -i gff < corCor.onlyCDS.gff > corCor.onlyCDS.gff.bed
+cat corCor.onlyCDS.gff.bed | python corCor.genenames.py > corCor.onlyCDS.genes.bed
+
 bedtools intersect -a corCor.vcfann.bed -b _Ccornix_clean_coverage_sites_merged.bed -wa > corCor.filtered.bed
 bedtools intersect -a corCor.onlyCDS.genes.bed -b corCor.filtered.bed -wa -wb > corCor.final.bed 
 cat corCor.final.bed | cut -f1,2,3,4,8 > corCor.final.clean.bed

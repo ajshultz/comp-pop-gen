dd216f7326671f4a18e5128c346130d5d1b76176
Sara JS Wuitchik <50593708+sjswuitchik@users.noreply.github.com>
2019-08-29 15:12:25 -0400

    Update and rename 07_final_tables.R to 06_final_tables.R

--- pipeline_scripts/mk_testing/06_final_tables.R
+++ pipeline_scripts/mk_testing/06_final_tables.R
@@ -1,3 +1,14 @@
+
+cds <- read.delim("corCor.onlyCDS.genes.bed", sep = "\t", header = F, stringsAsFactors = F, quote = "") %>%
+  as_tibble()
+colnames(cds) <- c("chr", "start", "end", "gene")
+cds.temp <- cds %>%
+  mutate(cds.temp = end - start)
+cds.len <- cds.temp %>%
+  group_by(gene) %>% 
+  summarise(cds.len = sum(cds.temp))
+write.table(cds.len, "cdslen.txt", sep = "\t", row.names = F, quote = F)
+
 call <- read.delim("corCor.callable.bed", sep = "\t", header = F, stringsAsFactors = F, quote = "") %>% as_tibble()
 colnames(call) <- c("chr", "start", "end", "gene")
 call.temp <- call %>% mutate(call.temp = end - start)
@@ -14,13 +25,14 @@ syn <- data %>%
   group_by(gene) %>%
   tally(effect == "synonymous_variant")
 colnames(syn) <- c("gene", "syn")
+
 table <- left_join(mis, syn, by = "gene")
-cds <- read.delim("cdslen.txt", sep = "\t", stringsAsFactors = F, quote = "") %>% as_tibble()
-tab2 <- left_join(table, cds, by = "gene")
-call <- read.delim("call.txt", sep = "\t", stringsAsFactors = F, quote = "") %>% as_tibble()
+tab2 <- left_join(table, cds.len, by = "gene")
 final <- left_join(tab2, call, by = "gene") %>% select(gene, cds.len, call.len, syn, mis)
+
+# qc: make sure no ratios > 1
 check <- final %>% mutate(check = call.len/cds.len)
 check[!complete.cases(check),]
 max(check$check)
 write.table(final, "corCor.table.txt", sep = "\t", row.names = F, quote = F)
-write.table(check, "corCor.table_check.txt", sep = "\t", row.names = F, quote = F)
+# write.table(check, "corCor.table_check.txt", sep = "\t", row.names = F, quote = F)

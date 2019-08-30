1a590e892832d068bdf7a0c46ed741439acff31c
Sara JS Wuitchik <50593708+sjswuitchik@users.noreply.github.com>
2019-08-30 10:41:38 -0400

    Update 07_final_tables.R

--- pipeline_scripts/mk_testing/07_final_tables.R
+++ pipeline_scripts/mk_testing/07_final_tables.R
@@ -13,7 +13,7 @@ write.table(cds.len, "cdslen.txt", sep = "\t", row.names = F, quote = F)
 call <- read.delim("corCor.callable.bed", sep = "\t", header = F, stringsAsFactors = F, quote = "") %>% as_tibble()
 colnames(call) <- c("chr", "start", "end", "gene")
 call.temp <- call %>% mutate(call.temp = end - start)
-call.len <- call.temp %>% group_by(gene) %>% summarise(call.len = sum(call.temp))
+call.length <- call.temp %>% group_by(gene) %>% summarise(call.len = sum(call.temp))
 write.table(call.len, "call.txt", sep = "\t", row.names = F, quote = F)
 
 data <- read.delim("corCor.final.clean.bed", sep = "\t", header = F, stringsAsFactors = F, quote = "") %>% as_tibble()
@@ -29,10 +29,12 @@ colnames(syn) <- c("gene", "syn")
 
 table <- left_join(mis, syn, by = "gene")
 tab2 <- left_join(table, cds.len, by = "gene")
-final <- left_join(tab2, call, by = "gene") %>% select(gene, cds.len, call.len, syn, mis)
+final <- left_join(tab2, call, by = "gene") %>% 
+  select(gene, cds.len, call.length, syn, mis)
 
 # qc: make sure no ratios > 1
-check <- final %>% mutate(check = call.len/cds.len)
+check <- final %>% 
+  mutate(check = call.len/cds.len)
 check[!complete.cases(check),]
 max(check$check)
 write.table(final, "corCor.table.txt", sep = "\t", row.names = F, quote = F)

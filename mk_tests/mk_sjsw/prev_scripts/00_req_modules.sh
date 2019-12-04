97cc0d1cccd67ad1f19b43c43d0bce9e1f2c5db4
sjswuitchik <sjswuit@g.harvard.edu>
2019-12-04 14:54:30 -0500

    renamed directory

--- mk_tests/mk_sjsw/prev_scripts/00_req_modules.sh
+++
@@ -1,7 +0,0 @@
-## Modules required for pipeline scripts on cluster
-
-module load bcftools/1.5-fasrc02 samtools/1.5-fasrc02 vcftools/0.1.14-fasrc01 htslib/1.5-fasrc02 jdk/10.0.1-fasrc01 gcc/7.1.0-fasrc01 bedops/2.4.25-fasrc01 bedtools2/2.26.0-fasrc01 python/3.6.3-fasrc01
-
-## if using an updated version of R, will need to reload dependencies
-module load gcc/8.2.0-fasrc01 openmpi/3.1.1-fasrc01 R/3.6.1-fasrc01
-
